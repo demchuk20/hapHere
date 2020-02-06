2df472ce5e940344762cc107acb1c9d574e980a6
Юрій Демчук <yura.demchuk.20@gmail.com>
2020-02-06 02:51:26 +0200

    test

--- hapHere-service/src/main/java/com/project/hapHere/HapHereApplication.java
+++ hapHere-service/src/main/java/com/project/hapHere/HapHereApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HapHereApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HapHereApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HapHereApplication.class, args);
+    }
 
 }
